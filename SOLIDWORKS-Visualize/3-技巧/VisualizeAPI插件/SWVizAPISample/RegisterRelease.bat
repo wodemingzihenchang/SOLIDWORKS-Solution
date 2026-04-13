a981e754b37c18ed4e2f826830fd62250e8b420d
wodemingzihenchang <735909224@qq.com>
2026-04-13 23:38:06 +0800

    Site updated: 2026-04-13 23:37:44

--- SOLIDWORKS-Visualize/3-技巧/VisualizeAPI插件/SWVizAPISample/RegisterRelease.bat
+++
@@ -1,7 +0,0 @@
-set FMWK="v4.0.30319"
-
-CD "%~dp0"
-
-IF EXIST "%Windir%\Microsoft.NET\Framework64\%FMWK%\regasm.exe" "%Windir%\Microsoft.NET\Framework64\%FMWK%\regasm" /codebase "./bin/x64/Release/SWVizAPISample.dll" /tlb
-
-pause
